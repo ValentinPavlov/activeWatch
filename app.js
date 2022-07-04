558bfc69769c964b12a348f1d2bbb699dedee6da
ValentinPavlov <valentinpavlov38@gmail.com>
2022-07-04 20:28:52 +0300

    All conversations done

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-let watchTimeSet = () => {
-    let currentTime = new Date()
-    let hours = (currentTime.getHours() < 10) ? '0' + currentTime.getHours() : currentTime.getHours()
-    let minutes = (currentTime.getMinutes() < 10) ? '0' + currentTime.getMinutes() : currentTime.getMinutes();
+const watchTimeSet = () => {
+    const currentTime = new Date()
+    const year = currentTime.getFullYear()
+    const hours = (currentTime.getHours() < 10) ? '0' + currentTime.getHours() : currentTime.getHours()
+    const minutes = (currentTime.getMinutes() < 10) ? '0' + currentTime.getMinutes() : currentTime.getMinutes();
     document.querySelector('.hour').innerHTML = hours + ':' + minutes;
-
-}
-let watchYearSet = () => {
-    let currentDate = new Date();
-    let year = currentDate.getFullYear()
     document.querySelector('.watch_year').innerHTML = 'since. ' + year
 
 }
 setInterval(watchTimeSet, 100);
-setInterval(watchYearSet, 100);
